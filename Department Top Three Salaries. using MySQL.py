9fdcd617e99c4fd8189ca60e2b835d29604a3bdf
Ranil <raniltech18@gmail.com>
2023-04-27 20:41:55 +0530

    Create Department Top Three Salaries. using MySQL.py

---
+++ Department Top Three Salaries. using MySQL.py
@@ -0,0 +1,51 @@
+select d.name as department , e1.name as employee, e1.salary as Salary
+from Employee e1 join Department d on e1.DepartmentId = d.Id
+where  3 > (select count(distinct (e2.Salary))
+        from  Employee e2
+        where e2.Salary > e1.Salary
+            and e1.DepartmentId = e2.DepartmentId)# is the code by ranil
+print("done by ranil")
+            
+Example 1:
+
+Input: 
+Employee table:
++----+-------+--------+--------------+
+| id | name  | salary | departmentId |
++----+-------+--------+--------------+
+| 1  | Joe   | 85000  | 1            |
+| 2  | Henry | 80000  | 2            |
+| 3  | Sam   | 60000  | 2            |
+| 4  | Max   | 90000  | 1            |
+| 5  | Janet | 69000  | 1            |
+| 6  | Randy | 85000  | 1            |
+| 7  | Will  | 70000  | 1            |
++----+-------+--------+--------------+
+Department table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | IT    |
+| 2  | Sales |
++----+-------+
+Output: 
++------------+----------+--------+
+| Department | Employee | Salary |
++------------+----------+--------+
+| IT         | Max      | 90000  |
+| IT         | Joe      | 85000  |
+| IT         | Randy    | 85000  |
+| IT         | Will     | 70000  |
+| Sales      | Henry    | 80000  |
+| Sales      | Sam      | 60000  |
++------------+----------+--------+
+Explanation: 
+In the IT department:
+- Max earns the highest unique salary
+- Both Randy and Joe earn the second-highest unique salary
+- Will earns the third-highest unique salary
+
+In the Sales department:
+- Henry earns the highest salary
+- Sam earns the second-highest salary
+- There is no third-highest salary as there are only two employees

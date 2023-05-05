b4173fe06ffaf5bf3a16952a3f2fe3a7e67580e1
Ranil <raniltech18@gmail.com>
2023-05-05 21:39:44 +0530

    Create 1456. Maximum Number of Vowels in a Substring of Given Length.py

---
+++ 1456. Maximum Number of Vowels in a Substring of Given Length.py
@@ -0,0 +1,32 @@
+class Solution:
+    def maxVowels(self, s: str, k: int) -> int:
+        vowels = set(['a', 'e', 'i', 'o', 'u'])
+        max_vow = cur_vow = 0
+        for i, c in enumerate(s):
+            if c in vowels:
+                cur_vow += 1
+            if i >= k and s[i-k] in vowels:
+                cur_vow -= 1
+            max_vow = max(max_vow, cur_vow)
+        return max_vow
+# Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+
+# Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+ 
+
+# Example 1:
+
+# Input: s = "abciiidef", k = 3
+# Output: 3
+# Explanation: The substring "iii" contains 3 vowel letters.
+# Example 2:
+
+# Input: s = "aeiou", k = 2
+# Output: 2
+# Explanation: Any substring of length 2 contains 2 vowels.
+# Example 3:
+
+# Input: s = "leetcode", k = 3
+# Output: 2
+# Explanation: "lee", "eet" and "ode" contain 2 vowels.

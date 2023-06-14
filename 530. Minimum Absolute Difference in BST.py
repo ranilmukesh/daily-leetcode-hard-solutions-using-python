da7f3f536a56f8986b39133e2d2d4b9c27492bc2
Ranil <raniltech18@gmail.com>
2023-06-14 21:30:28 +0530

    Create 530. Minimum Absolute Difference in BST.py

---
+++ 530. Minimum Absolute Difference in BST.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
+        d = float('inf')
+        s = []
+        if root == None:
+            return 
+        d = self.traverse(root,d,s)
+        return d
+    def traverse(self,root,d,s):
+        if root.left != None:
+            d = self.traverse(root.left,d,s)
+        s.append(root.val)
+        if len(s)>1:
+            diff = s[-1]-s[-2]
+            if diff < d:
+                d = diff
+        if root.right != None:
+            d = self.traverse(root.right,d,s) 
+        return d

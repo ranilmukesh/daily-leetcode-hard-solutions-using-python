5a92727809bcf3862b4a49dbc5dfbd78368f5b22
Ranil <raniltech18@gmail.com>
2023-10-15 11:20:24 +0530

    Create 1269. Number of Ways to Stay in the Same Place After Some Steps.py

---
+++ 1269. Number of Ways to Stay in the Same Place After Some Steps.py
@@ -0,0 +1,10 @@
+class Solution:
+    def numWays(self, steps, arrLen):
+        n = min(steps // 2 + 1, arrLen)
+        mod = 10 ** 9 + 7
+        dp = [1] + [0] * n
+        for _ in range(steps):
+            prev = 0
+            for i in range(n):
+                dp[i], prev = (prev + dp[i] + dp[i + 1]) % mod, dp[i]
+        return dp[0]

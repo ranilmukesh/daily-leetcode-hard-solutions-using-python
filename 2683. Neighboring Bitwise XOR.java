73fead74ac851e773a93d6edda1920f8303c42b9
Ranil <raniltech18@gmail.com>
2025-01-17 23:34:38 +0530

    Create 2683. Neighboring Bitwise XOR.java

---
+++ 2683. Neighboring Bitwise XOR.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean doesValidArrayExist(int[] derived) {
+        int xor = 0;
+
+        for(int n : derived) {
+            xor ^= n;
+        }
+
+        return xor == 0;
+    }
+}

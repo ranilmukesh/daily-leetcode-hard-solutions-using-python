73effb7119e621b088d209e3e08474878a4c7af0
Ranil Mukesh MJ <raniltech18@gmail.com>
2025-06-18 23:12:51 +0530

    Create 2966. Divide Array Into Arrays With Max Difference.java

---
+++ 2966. Divide Array Into Arrays With Max Difference.java
@@ -0,0 +1,11 @@
+class Solution:
+    def divideArray(self, nums: List[int], k: int) -> List[List[int]]:
+        n = len(nums)
+        nums.sort()
+        res = []
+        for i in range(0, n, 3):
+            a, b, c = nums[i], nums[i+1], nums[i+2]
+            if c - a > k:
+                return []
+            res.append([a, b, c])
+        return res

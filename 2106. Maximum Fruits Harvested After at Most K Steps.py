d50fcfadcc8d54e40a551e1835cdbf70ab0463d8
Ranil Mukesh MJ <raniltech18@gmail.com>
2025-08-03 23:08:21 +0530

    Create 2106. Maximum Fruits Harvested After at Most K Steps.py

---
+++ 2106. Maximum Fruits Harvested After at Most K Steps.py
@@ -0,0 +1,27 @@
+class Solution:
+    def maxTotalFruits(self, fruits: List[List[int]], startPos: int, k: int) -> int:
+
+        max_res = 0
+        left = bisect.bisect_left(fruits, [startPos - k, float("-inf")]) # >=
+        right = bisect.bisect_right(fruits, [startPos, float("inf")]) # >
+
+
+        curr_subarray_sum = sum([fruits[i][1] for i in range(left, right)])
+        max_res = curr_subarray_sum
+
+        for i in range(right, len(fruits)):
+            curr_subarray_sum += fruits[i][1]
+            right_loc = fruits[i][0]
+
+            if right_loc - startPos > k:
+                break
+
+            while min(abs(right_loc - startPos), abs(startPos- fruits[left][0])) + right_loc - fruits[left][0] > k:
+                curr_subarray_sum -= fruits[left][1]
+                left += 1
+            max_res = max(max_res, curr_subarray_sum)
+        
+        return max_res
+
+
+            

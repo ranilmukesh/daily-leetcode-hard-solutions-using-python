ac27ae305b22faecd2074848c034e8faa9bdc2e8
Ranil Mukesh MJ <raniltech18@gmail.com>
2025-08-14 15:02:15 +0530

    Create 2264. Largest 3-Same-Digit Number in String.java

---
+++ 2264. Largest 3-Same-Digit Number in String.java
@@ -0,0 +1,18 @@
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        c = 0
+        cur = None
+        max_good = -1
+        for char in num:
+            if char == cur:
+                c += 1
+                if c == 3 and int(cur) > max_good:
+                    max_good = int(cur)
+            else:
+                cur = char
+                c = 1
+        if max_good < 0:
+            return ""
+        return str(max_good) * 3
+        
+        

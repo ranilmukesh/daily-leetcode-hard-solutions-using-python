52b86804b0b4235719222795274591c38847452b
Ranil Mukesh MJ <raniltech18@gmail.com>
2025-12-01 23:34:13 +0530

    Create Maximum Running Time of N Computers.java

---
+++ Maximum Running Time of N Computers.java
@@ -0,0 +1,16 @@
+class Solution:
+        
+    def maxRunTime(self, n: int, batteries: List[int]) -> int:
+        m = len(batteries)
+        if n==m:
+            return min(batteries)
+        if m < n:
+            return 0
+        batteries.sort()
+        S = sum(batteries[:-n])
+        L = batteries[-n:] 
+        for i in range(n-1):
+            if S < (i+1) * (L[i+1] - L[i]):
+                return L[i] + S // (i+1)
+            S -= (i+1) * (L[i+1] - L[i])
+        return L[-1] + S // n

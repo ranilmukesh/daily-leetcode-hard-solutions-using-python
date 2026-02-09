2598ee9c1cea2b7a209e785b0309168a632e6fa6
Ranil Mukesh MJ <raniltech18@gmail.com>
2026-02-09 18:18:31 +0530

    Implement function to balance a binary search tree

---
+++ Balance a Binary Search Tree.java
@@ -0,0 +1,22 @@
+class Solution {
+    ArrayList<TreeNode> res = new ArrayList<TreeNode>();
+    public TreeNode balanceBST(TreeNode root) {
+         inorder(root);
+        return Build_BST(0, res.size() - 1);
+    }
+    public void inorder(TreeNode root ){
+         if(root == null) return;
+         inorder(root.left);
+         res.add(root);
+         inorder(root.right);
+    }
+    public TreeNode Build_BST( int start, int end){
+        if(start > end) return null;
+        int mid = (start + end) / 2;
+        TreeNode node = res.get(mid);
+        node.left = Build_BST( start, mid - 1);
+        node.right = Build_BST( mid + 1, end);
+        return node;
+    }
+
+}
